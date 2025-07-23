4d3c802444c9aa64f28817b5b98a64c39023ce10
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-07-23 17:59:08 +0530

    Add Special, CouponCode Property

--- Coupoun_API/Models/Coupon.cs
+++ Coupoun_API/Models/Coupon.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Coupoun_API.Models
+namespace Coupon_API.Models
 {
     public class Coupon
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string CouponCode { get; set; }
         public int Percentage { get; set; }
         public DateTime ExpireDate { get; set; }
 
@@ -17,12 +18,28 @@ namespace Coupoun_API.Models
             }
         }
 
+        public bool IsSpecial { get; set; } = false;
+
+        public bool isValid
+        {
+            get
+            {
+                return ExpireDate > DateTime.Now && IsActive;
+            }
+        }
+
         public bool IsActive { get; set; }
+
         public int CreateId { get; set; }
+
         public DateTime CreateDate { get; set; } = DateTime.Now;
+
         public int ModifyId { get; set; }
+
         public DateTime ModifyDate { get; set; }
+
         public int DeleteId { get; set; }
+
         public DateTime DeleteDate { get; set; }
     }
 }
