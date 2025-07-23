a4397815ee8ade1db538d86fefc9f4a453c18261
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-07-23 15:52:27 +0530

    Add project files.

---
+++ Coupoun_API/Models/Coupon.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Coupoun_API.Models
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Percentage { get; set; }
+        public DateTime ExpireDate { get; set; }
+
+        public int ValidDays
+        {
+            get
+            {
+                return (ExpireDate - CreateDate).Days;
+            }
+        }
+
+        public bool IsActive { get; set; }
+        public int CreateId { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public int ModifyId { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public int DeleteId { get; set; }
+        public DateTime DeleteDate { get; set; }
+    }
+}
