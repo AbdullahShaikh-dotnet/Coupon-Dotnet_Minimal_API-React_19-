f82a6f17cb5cc7defc84d1f058fe00eb9e1ff00e
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-07-25 15:08:51 +0530

    Added DTO (Data Transfer Object)

---
+++ Coupoun_API/Models/DTO/CouponCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Coupon_API.Models.DTO
+{
+    public class CouponCreateDTO
+    {
+        public string Name { get; set; }
+        public string CouponCode { get; set; }
+        public int Percentage { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
---
+++ Coupoun_API/Models/DTO/CouponDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace Coupon_API.Models.DTO
+{
+    public class CouponDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CouponCode { get; set; }
+        public int Percentage { get; set; }
+        public DateTime ExpireDate { get; set; }
+
+        public int ValidDays
+        {
+            get
+            {
+                return (ExpireDate - CreateDate).Days;
+            }
+        }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreateDate { get; set; }
+
+        public bool isValid
+        {
+            get
+            {
+                return ExpireDate > DateTime.Now && IsActive;
+            }
+        }
+    }
+}
