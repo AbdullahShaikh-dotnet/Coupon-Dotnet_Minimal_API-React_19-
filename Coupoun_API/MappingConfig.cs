6a52b834a04d189ced0573a9e55ea21d593ec828
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-07-25 17:16:57 +0530

    Configuration Created for Automapper

---
+++ Coupoun_API/MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Coupon_API.Models;
+using Coupon_API.Models.DTO;
+
+namespace Coupon_API
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Coupon, CouponCreateDTO>().ReverseMap();
+            CreateMap<Coupon, CouponDTO>().ReverseMap();
+        }
+    }
+}
