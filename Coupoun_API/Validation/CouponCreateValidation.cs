887a5ea0b6131a7c5349fa26c2ea7fa6f91636d0
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-07-25 17:48:51 +0530

    Implemented Coupon Create Validation

---
+++ Coupoun_API/Validation/CouponCreateValidation.cs
@@ -0,0 +1,20 @@
+﻿using Coupon_API.Models.DTO;
+using FluentValidation;
+
+
+namespace Coupon_API.Validation
+{
+    public class CouponCreateValidation : AbstractValidator<CouponCreateDTO>
+    {
+        public CouponCreateValidation()
+        {
+            RuleFor(model => model.Name).NotEmpty().WithMessage("Coupon Name is required.")
+                .Length(3, 150).WithMessage("Coupon Name must be between 3 and 50 characters.");
+
+            RuleFor(model => model.CouponCode).NotEmpty().WithMessage("Coupon Code is required.")
+                .Length(3, 50).WithMessage("Coupon Code must be between 3 and 50 characters.");
+
+            RuleFor(model => model.Percentage).InclusiveBetween(1, 100).WithMessage("Percentage must be between 1 to 100");
+        }
+    }
+}
