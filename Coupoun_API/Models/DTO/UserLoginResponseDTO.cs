006de2dbe7a807f36902159dbefefbe397bf6a40
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-08-02 12:55:24 +0530

    Added all the DTO for User

---
+++ Coupoun_API/Models/DTO/UserLoginResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Coupon_API.Models.DTO
+{
+    public class UserLoginResponseDTO
+    {
+        public UserDTO User { get; set; }
+
+        public string Token { get; set; }
+    }
+}
+// This class represents the response returned after a user logs in, containing the user details and an authentication token.
---
+++ Coupoun_API/Models/DTO/UserRegisterDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Coupon_API.Models.DTO
+{
+    public class UserRegisterDTO
+    {
+        public string UserName { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+    }
+}
