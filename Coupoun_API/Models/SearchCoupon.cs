ae6703be7cd7cf69d85742cd5a294892bb04c4c0
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-08-04 13:21:15 +0530

    Added Search Coupon Model

---
+++ Coupoun_API/Models/SearchCoupon.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Coupon_API.Models
+{
+    public class SearchCoupon
+    {
+        public string CouponName { get; set; }
+
+        public int PageSize { get; set; } = 10;
+
+        public int PageNumber { get; set; } = 1;
+    }
+}
