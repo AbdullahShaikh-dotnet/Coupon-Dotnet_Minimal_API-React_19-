01635ec0172519b44872fbca57159a69bd67926a
abdullah034 <shaikhabdullah299@gmail.com>
2025-08-07 01:15:24 +0530

    Added React Router for Component Routing

--- coupon-ui/src/main.jsx
+++ coupon-ui/src/main.jsx
@@ -2,9 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet
+} from "react-router";
+import Login from './Components/Login';
+import Register from './Components/Register';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// eslint-disable-next-line react-refresh/only-export-components
+const AppLayout = () => {
+    return <>
+        <h1>Main</h1>
+        <Outlet />
+    </>
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/Login",
+                element: <Login />,
+            },
+
+            {
+                path: "/Register",
+                element: <Register />,
+            },
+        ]
+    },
+]);
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <RouterProvider router={router} />
 )
