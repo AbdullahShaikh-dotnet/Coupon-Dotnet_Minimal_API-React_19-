c98ffd31b2cecfc482a69205770694188cfa5750
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-08-06 16:03:38 +0530

    Created Empty Login & Register Component

---
+++ coupon-ui/src/Components/Register.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+    const [form, setForm] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+  return (
+
+  );
+};
+
+export default Register;
