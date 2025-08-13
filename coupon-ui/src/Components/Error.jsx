451b79a5d91f70eefcab3bc3fc8434bdfabbe381
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-08-13 18:22:04 +0530

    Added Error Component

---
+++ coupon-ui/src/Components/Error.jsx
@@ -0,0 +1,20 @@
+import { Link, useRouteError } from "react-router";
+
+const Error = () => {
+    const Errors = useRouteError();
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-red-600">
+            <div className="p-8 flex flex-col items-center max-w-md w-full">
+                <h1 className="text-9xl font-bold text-white mb-4">{Errors.status}</h1>
+                <p className="text-3xl text-white mb-2">{Errors.statusText || "Something went wrong."}</p>
+                <p className="text-white font-semibold text-xl">Please check the URL or try again later.</p>
+            </div>
+
+            <Link to={"/"}>
+                <div className="bg-white p-1 rounded-md px-4 text-md font-semibold text-gray-600">Back to Home</div>
+            </Link>
+        </div>
+    )
+}
+
+export default Error;
