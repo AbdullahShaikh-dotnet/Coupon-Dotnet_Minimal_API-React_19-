167c1f2fe9b543f09b05a1dea88eb7aa5b166938
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-08-14 16:53:34 +0530

    Added Home Path and set Context from local storage

--- coupon-ui/src/main.jsx
+++ coupon-ui/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
@@ -6,15 +6,26 @@ import Login from './Components/Login';
 import Register from './Components/Register';
 import Error from './Components/Error';
 import userContext from '../src/Utility/UserContext'
+import Home from './Components/Home';
+import Header from './Components/Header';
 
 const AppLayout = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
     return <>
-        <userContext.Provider value={{ user: null, setUser: () => {}, isLoggedIn: false }}>
+        <userContext.Provider value={{ user, setUser, isLoggedIn: !!user }}>
+            <Header/>
             <Outlet />
         </userContext.Provider>
     </>
 }
-
 
 const router = createBrowserRouter([
     {
@@ -37,6 +48,10 @@ const router = createBrowserRouter([
             {
                 path: "/main/register",
                 element: <Register />,
+            },
+            {
+                path: "/main/home",
+                element: <Home />,
             },
         ]
     }
