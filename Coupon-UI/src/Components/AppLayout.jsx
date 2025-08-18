bd74f44468b614bfea25a66dede26d67e59944a5
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-08-18 12:56:14 +0530

    Fixed Styling for Header Component

--- Coupon-UI/src/Components/AppLayout.jsx
+++ Coupon-UI/src/Components/AppLayout.jsx
@@ -4,23 +4,23 @@ import UserContext from "../Utility/UserContext";
 import { Outlet } from "react-router";
 
 const AppLayout = () => {
-  const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
 
-  return (
-    <>
-      <UserContext value={{ user, setUser, isLoggedIn: !!user }}>
-        <Header />
-        <Outlet />
-      </UserContext>
-    </>
-  );
+    return (
+        <>
+            <UserContext value={{ user, setUser, isLoggedIn: !!user }}>
+                <Header className="fixed top-0 left-0 w-full h-16 bg-gray-800 text-white flex items-center px-4" />
+                <Outlet />
+            </UserContext>
+        </>
+    );
 };
 
 
