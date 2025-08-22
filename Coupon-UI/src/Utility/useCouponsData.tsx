4f2f3081bf623c0ccc630af3bcd20d85a7e10268
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-08-22 18:34:06 +0530

    Added Custome Hook to retrive Coupon Data by ID

--- Coupon-UI/src/Utility/useCouponsData.tsx
+++ Coupon-UI/src/Utility/useCouponsData.tsx
@@ -1,31 +1,59 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-function useCouponsData() {
-    const [data, setData] = useState<any[]>([]);
-    const [error, setError] = useState<Error | null>(null);
-    const [isLoading, setLoading] = useState(true);
+export function useCouponsData() {
+  const [data, setData] = useState<any[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch('/api/coupon') || await fetch('/API/coupon');
-                const GetCouponData = await res.json();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res =
+          (await fetch("/api/coupon")) || (await fetch("/API/coupon"));
+        const GetCouponData = await res.json();
 
-                if (!GetCouponData.isSuccess) {
-                    throw new Error(GetCouponData.errorMessages.join(', '));
-                }
+        if (!GetCouponData.isSuccess) {
+          throw new Error(GetCouponData.errorMessages.join(", "));
+        }
 
-                setData(GetCouponData.result);
-                setLoading(false);
-            } catch (err: any) {
-                setError(err || new Error("Something went wrong !!"));
-            }
-        };
+        setData(GetCouponData.result);
+        setLoading(false);
+      } catch (err: any) {
+        setError(err || new Error("Something went wrong !!"));
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return { data, error, isLoading };
+  return { data, error, isLoading };
 }
 
-export default useCouponsData;
+export function useCouponsDataByID(id: number) {
+  const [data, setData] = useState<any[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res =
+          (await fetch(`/api/coupon/${id}`)) || (await fetch(`/API/coupon/${id}`));
+        const GetCouponData = await res.json();
+
+        if (!GetCouponData.isSuccess) {
+          throw new Error(GetCouponData.errorMessages.join(", "));
+        }
+
+        setData(GetCouponData.result[0]);
+        setLoading(false);
+      } catch (err: any) {
+        setError(err || new Error("Something went wrong !!"));
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, error, isLoading };
+}
