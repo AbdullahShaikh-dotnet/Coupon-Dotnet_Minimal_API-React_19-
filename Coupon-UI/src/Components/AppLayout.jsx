f0a579f637ee19dd16dddd3dfef185ec20cafb85
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-08-23 17:25:38 +0530

    Wrap Outlet with offline fallback Component

--- Coupon-UI/src/Components/AppLayout.jsx
+++ Coupon-UI/src/Components/AppLayout.jsx
@@ -2,46 +2,50 @@ import Header from "./Header";
 import { useState, useEffect } from "react";
 import UserContext from "../Utility/UserContext";
 import { Outlet, useNavigate } from "react-router";
-import { Progress } from "@/components/ui/progress"; // shadcn progress import
+import { Progress } from "@/Components/ui/progress"; // shadcn progress import
+import { OfflineFallback } from "@/Components/offline-fallback"
+
 
 const AppLayout = () => {
-  const navigate = useNavigate();
-  const [progress, setProgress] = useState(10);
+    const navigate = useNavigate();
+    const [progress, setProgress] = useState(10);
 
-  // Check if user is stored in localStorage or sessionStorage
-  const [user, setUser] = useState(() => {
-    const storedUserObject = localStorage.getItem("user");
-    return storedUserObject ? JSON.parse(storedUserObject) : null;
-  });
+    // Check if user is stored in localStorage or sessionStorage
+    const [user, setUser] = useState(() => {
+        const storedUserObject = localStorage.getItem("user");
+        return storedUserObject ? JSON.parse(storedUserObject) : null;
+    });
 
-  useEffect(() => {
+    useEffect(() => {
+        if (!user) {
+            navigate("/login", { replace: true });
+        }
+
+        const timer = setInterval(() => {
+            setProgress((prev) => (prev >= 100 ? 10 : prev + 10));
+        }, 500);
+        return () => clearInterval(timer);
+
+    }, [user, navigate]);
+
     if (!user) {
-      navigate("/login", { replace: true });
+        return (
+            <div className="flex items-center justify-center w-screen h-screen bg-white">
+                <div className="w-64">
+                    <Progress value={progress} className="w-full" />
+                </div>
+            </div>
+        );
     }
 
-    const timer = setInterval(() => {
-      setProgress((prev) => (prev >= 100 ? 10 : prev + 10));
-    }, 500);
-    return () => clearInterval(timer);
-
-  }, [user, navigate]);
-
-  if (!user) {
     return (
-      <div className="flex items-center justify-center w-screen h-screen bg-white">
-        <div className="w-64">
-          <Progress value={progress} className="w-full" />
-        </div>
-      </div>
+        <UserContext.Provider value={{ user, setUser, isLoggedIn: !!user }}>
+            <Header className="fixed top-0 left-0 w-full h-16 bg-gray-800 text-white flex items-center px-4" />
+            <OfflineFallback>
+                <Outlet />
+            </OfflineFallback>
+        </UserContext.Provider>
     );
-  }
-
-  return (
-    <UserContext.Provider value={{ user, setUser, isLoggedIn: !!user }}>
-      <Header className="fixed top-0 left-0 w-full h-16 bg-gray-800 text-white flex items-center px-4" />
-      <Outlet />
-    </UserContext.Provider>
-  );
 };
 
 export default AppLayout;
