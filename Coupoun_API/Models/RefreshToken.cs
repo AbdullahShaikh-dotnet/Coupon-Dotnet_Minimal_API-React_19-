4e8196eeab45f9e9791d19f6ec92462fc2286d60
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-08-29 17:04:12 +0530

    Added JWT Refresh Token Model

---
+++ Coupoun_API/Models/RefreshToken.cs
@@ -0,0 +1,14 @@
+﻿namespace Coupon_API.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime ExpiryDate { get; set; }
+        public bool IsUsed { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
