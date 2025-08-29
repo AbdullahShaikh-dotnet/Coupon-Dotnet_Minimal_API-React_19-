a7d98fc31b4bff90fec69e4b1c7990c5a65f13d6
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-08-29 17:14:23 +0530

    Added Fluent Validation for Refresh Token

---
+++ Coupoun_API/Validation/RefreshTokenDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using Coupon_API.Models.DTO;
+using FluentValidation;
+
+namespace Coupon_API.Validation
+{
+    public class RefreshTokenDtoValidator : AbstractValidator<RefreshTokenDto>
+    {
+        public RefreshTokenDtoValidator()
+        {
+            RuleFor(x => x.Token)
+                .NotEmpty()
+                .WithMessage("Access token is required")
+                .MinimumLength(50)
+                .WithMessage("Invalid access token format");
+
+            RuleFor(x => x.RefreshToken)
+                .NotEmpty()
+                .WithMessage("Refresh token is required")
+                .MinimumLength(20)
+                .WithMessage("Invalid refresh token format");
+        }
+    }
+}
