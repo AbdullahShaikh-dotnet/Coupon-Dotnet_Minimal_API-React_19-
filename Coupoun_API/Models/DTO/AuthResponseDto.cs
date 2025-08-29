b5854a6842b46bfa295c419f27af7e02c285e6dd
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-08-29 17:08:50 +0530

    Added Auth Response and Refresh Token DTO

---
+++ Coupoun_API/Models/DTO/AuthResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Coupon_API.Models.DTO
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
+        public UserLoginResponseDTO User { get; set; } = null!;
+    }
+}
---
+++ Coupoun_API/Models/DTO/RefreshTokenDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Coupon_API.Models.DTO
+{
+    public class RefreshTokenDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
