8a02cc56e23653f581db7f21d0f0b325635ab7b4
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-08-29 17:05:14 +0530

    Register Refresh Token Model in Application DB context

--- Coupoun_API/Data/ApplicationDbContext.cs
+++ Coupoun_API/Data/ApplicationDbContext.cs
@@ -13,5 +13,21 @@ namespace Coupon_API.Data
         public DbSet<Coupon> Coupons { get; set; }
         public DbSet<User> Users { get; set; }
 
+        public DbSet<RefreshToken> RefreshTokens { get; set; } // Add this
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RefreshToken>(entity =>
+            {
+                entity.HasKey(rt => rt.Id);
+                entity.Property(rt => rt.Token).IsRequired().HasMaxLength(500);
+                entity.HasIndex(rt => rt.Token).IsUnique();
+                entity.HasOne(rt => rt.User)
+                      .WithMany()
+                      .HasForeignKey(rt => rt.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+
     }
 }
