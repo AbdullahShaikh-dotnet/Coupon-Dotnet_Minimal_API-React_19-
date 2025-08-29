77283c7c934eb964ef413cf8156d06dbee5b30fb
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-08-29 17:11:01 +0530

    Added Secure Login and Refresh Token Endpoints

--- Coupoun_API/EndPoints/Auth_EndPoints.cs
+++ Coupoun_API/EndPoints/Auth_EndPoints.cs
@@ -3,6 +3,7 @@ using Coupon_API.Data;
 using Coupon_API.Models;
 using Coupon_API.Models.DTO;
 using Coupon_API.Repository.IRepository;
+using Coupon_API.Services.Interfaces;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,7 +16,7 @@ namespace Coupon_API.EndPoints
     {
         public static void MapAuthEndpoints(this IEndpointRouteBuilder app)
         {
-            app.MapPost("/api/login", Login).WithName("Login")
+            app.MapPost("/api/login", SecureLogin).WithName("Login")
             .Produces<APIResponse>(201)
             .Produces(400)
             .Accepts<UserLoginDTO>(contentType: "application/json");
@@ -25,6 +26,12 @@ namespace Coupon_API.EndPoints
             .Produces<APIResponse>(201)
             .Produces(400)
             .Accepts<UserRegisterDTO>(contentType: "application/json");
+
+
+            app.MapPost("/api/auth/refresh", RefreshToken)
+            .WithName("RefreshToken")
+            .WithOpenApi()
+            .AllowAnonymous();
 
         }
         private static async Task<IResult> Login(IAuthRepository authRepository, [FromBody] UserLoginDTO userLogin)
@@ -47,9 +54,6 @@ namespace Coupon_API.EndPoints
 
             return Results.Ok(response);
         }
-
-
-
         private static async Task<IResult> Register(IAuthRepository authRepository, [FromBody] UserRegisterDTO userRegister)
         {
             APIResponse response = new APIResponse();
@@ -69,6 +73,38 @@ namespace Coupon_API.EndPoints
             response.IsSuccess = true;
 
             return Results.Ok(response);
+        }
+
+
+        private static async Task<IResult> SecureLogin(IAuthRepository authRepository, [FromBody] UserLoginDTO userLogin)
+        {
+            var loginResponse = await authRepository.LoginAsync(userLogin);
+
+            return loginResponse.Success ? Results.Ok(loginResponse) : Results.BadRequest(loginResponse);
+        }
+
+
+
+        private static async Task<IResult> RefreshToken(RefreshTokenDto refreshTokenDto, IAuthService authService, IValidator<RefreshTokenDto> validator)
+        {
+            var validationResult = await validator.ValidateAsync(refreshTokenDto);
+            if (!validationResult.IsValid)
+            {
+                var errors = validationResult.Errors.Select(e => e.ErrorMessage);
+                return Results.BadRequest(new APIResponse()
+                {
+                    ErrorMessages = errors.ToList(),
+                    IsSuccess = false,
+                    Result = "Validation failed",
+                    StatusCode = HttpStatusCode.Unauthorized
+                });
+            }
+
+            var result = await authService.RefreshTokenAsync(refreshTokenDto);
+
+            return result.Success
+                ? Results.Ok(result)
+                : Results.BadRequest(result);
         }
 
 
