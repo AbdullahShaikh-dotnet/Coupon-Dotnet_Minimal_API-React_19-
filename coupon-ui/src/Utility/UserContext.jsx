a4332d38fed84d35eede8c1d068333e8ac21cf01
ABDULLAH <72824454+AbdullahShaikh-dotnet@users.noreply.github.com>
2025-09-01 19:00:36 +0530

    Added Token details refreshToken get and set values for react Context

--- coupon-ui/src/Utility/UserContext.jsx
+++ coupon-ui/src/Utility/UserContext.jsx
@@ -4,6 +4,12 @@ const UserContext = createContext({
     user: null,
     setUser: () => {},
     isLoggedIn: false,
+    token: null,
+    refreshToken: null,
+    setToken: () => { },
+    setRefreshToken: () => { },
+    details: null,
+    setDetails: () => { },
 });
 
 export default UserContext;
